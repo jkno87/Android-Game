c3e4315f0e80ad1fb974e633d0da2654dd423d8a
jose <centaurkhan@gmail.com>
2015-10-06 18:02:36 -0500

    Se empezo con el cambio de la clase Drawer para que sea mas sencillo dibujar texturas. Falta hacer un test de la eficiencia.

--- app/src/main/java/com/jgame/util/Drawer.java
+++ app/src/main/java/com/jgame/util/Drawer.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
+import java.util.ArrayList;
 
 import javax.microedition.khronos.opengles.GL10;
 
@@ -24,6 +25,96 @@ public class Drawer {
     final ShortBuffer indices;
     final boolean withTexture;
     final boolean withColor;
+
+    private VertexQueue vertexQueue = new VertexQueue();
+    private int elementsAdded;
+
+    public static class VertexQueue {
+        private ArrayList<Float> accumVertices;
+
+        public VertexQueue(){
+            accumVertices = new ArrayList<>();
+        }
+
+        public VertexQueue addVertex(float e){
+            accumVertices.add(e);
+
+            return this;
+        }
+    }
+
+    public void addVertex(float e){
+        vertexQueue.addVertex(e);
+        elementsAdded++;
+    }
+
+    public void drawC(GL10 gl, boolean withTexture, boolean withColor){
+        if(elementsAdded == 0)
+            return;
+
+        int arraySize = (2 + (withTexture?2:0) + (withColor?4:0)) * VERTEX_PER_ELEMENT;
+        ByteBuffer buffer = ByteBuffer.allocateDirect(elementsAdded * arraySize * VERTEX_PER_ELEMENT);
+        buffer.order(ByteOrder.nativeOrder());
+        FloatBuffer vert = buffer.asFloatBuffer();
+
+        buffer = ByteBuffer.allocateDirect(elementsAdded * INDICES_PER_ELEMENT * Short.SIZE / 8);
+        buffer.order(ByteOrder.nativeOrder());
+        ShortBuffer ind = buffer.asShortBuffer();
+
+        short[] indices = new short[elementsAdded * INDICES_PER_ELEMENT];
+        int len = indices.length;
+        short j = 0;
+        for(int i = 0; i < len; i+= 6, j+= 4){
+            indices[i + 0] = (short)(j + 0);
+            indices[i + 1] = (short)(j + 1);
+            indices[i + 2] = (short)(j + 2);
+            indices[i + 3] = (short)(j + 2);
+            indices[i + 4] = (short)(j + 3);
+            indices[i + 5] = (short)(j + 0);
+        }
+
+        ind.clear();
+        ind.put(indices, 0, indices.length);
+        ind.flip();
+
+        float[] finalVertices = new float[elementsAdded * arraySize];
+
+        for(int n = 0; n < elementsAdded; n++)
+            finalVertices[n] = vertexQueue.accumVertices.get(n);
+
+        vert.clear();
+        vert.put(finalVertices, 0, elementsAdded);
+        vert.flip();
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        vert.position(0);
+        gl.glVertexPointer(2, GL10.GL_FLOAT, arraySize, vert);
+
+        if(withTexture){
+            gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+            vert.position(2);
+            gl.glTexCoordPointer(2, GL10.GL_FLOAT, arraySize, vert);
+        }
+
+        if(withColor){
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+            vertices.position(withTexture?4:2);
+            gl.glColorPointer(4, GL10.GL_FLOAT, arraySize, vertices);
+        }
+
+        ind.position(0);
+        gl.glDrawElements(GL10.GL_TRIANGLES, elementsAdded * INDICES_PER_ELEMENT, GL10.GL_UNSIGNED_SHORT, ind);
+
+        if(withTexture)
+            gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+
+        if(withColor)
+            gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
+
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+
+    }
+
 
     public Drawer(GL10 gl, int elems, boolean withTexture, boolean withColor){
         this.gl = gl;
