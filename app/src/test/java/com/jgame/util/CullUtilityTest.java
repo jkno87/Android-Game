5de5fb008ab4ff841ac30376d0710e3f6334de14
jose <centaurkhan@gmail.com>
2015-10-29 09:03:10 -0600

    Se corrigio un bug que existia en CullUtility para que termine correctamente la funcion de crear.

--- app/src/test/java/com/jgame/util/CullUtilityTest.java
+++ app/src/test/java/com/jgame/util/CullUtilityTest.java
@@ -11,7 +11,7 @@ public class CullUtilityTest {
     @Test
     public void simpleTest(){
         //TODO: Implementar el constructor de CullUtility utilizando las dimensiones de la grid 10 x 10 y el size original
-        CullUtility testInstance = new CullUtility(10, 320f, 480f);
+        CullUtility testInstance = new CullUtility(20, 320f, 480f);
         System.out.println(testInstance);
         assertTrue(true);
     }
