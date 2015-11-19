caecc6cc3ada187249bb4f7eeb51ccecf97924d6
jose <centaurkhan@gmail.com>
2015-11-19 09:39:50 -0600

    Se cambia la implementaci[on de MovingOrganism

--- app/src/main/java/com/jgame/elements/MovingOrganism.java
+++ app/src/main/java/com/jgame/elements/MovingOrganism.java
@@ -25,76 +25,59 @@ public class MovingOrganism extends Organism {
     public static final int FOOD_TO_EVOLVE = 10;
     public static final int DEFAULT_MOVES = 10;
     public State currentState;
-    private final TimeCounter timeToLive;
-    private int movesLeft;
-    private Vector2 direction;
-    private Vector2 position;
-    private Random random;
-    private final Circle sight;
-    public final Circle interaction;
     private float speedModifier;
     private int foodConsumed;
-    private float size;
     private OrganismBehavior initialBehavior;
 
-    public MovingOrganism(float timeToLive, Vector2 position, float sightDistance, float interactionDistance, float initialSize){
-        initialBehavior = new OrganismBehavior(timeToLive, new Circle(position, initialSize), HP, FOOD_POINTS, true) {
+    public MovingOrganism(float timeToLive, final Vector2 position, float sightDistance, final float interactionDistance){
+
+        initialBehavior = new OrganismBehavior(timeToLive, new Circle(position, sightDistance), HP, FOOD_POINTS, true) {
+            private int movesLeft = DEFAULT_MOVES;
+            private Vector2 direction = new Vector2();
+            private Random random = new Random();
+            private Circle organismBounds = new Circle(position, interactionDistance);
             @Override
             public void age(float timeDifference) {
+                if(movesLeft <= 0) {
+                    movesLeft = DEFAULT_MOVES;
+                    direction.set(random.nextInt(3) - 1, random.nextInt(3) - 1).nor();
+                    direction.mul(speedModifier);
+                }
 
+                movesLeft--;
+
+                position.add(direction);
+
+                if(position.x <= 0 || position.x >= GameLevels.FRUSTUM_WIDTH)
+                    direction.x *= -1;
+                if(position.y <= 0 || position.y >= GameLevels.MAX_PLAYING_HEIGHT)
+                    direction.y *= -1;
+
+                timeRemaining.accum(timeDifference);
             }
 
             @Override
             public void collide(GameElement e) {
+                if(e instanceof Organism){
+                    Organism o = (Organism) e;
+                    //Aqui falta hacer algo para que represente que consume comida
 
+                    if(foodConsumed > FOOD_TO_EVOLVE) {
+                        currentState = State.GROWING;
+                        movesLeft = DEFAULT_MOVES;
+                    }
+                }
             }
-        }
-    }
+        };
 
+        setBehavior(initialBehavior);
 
-    public MovingOrganism (float timeToLive, Vector2 position, float sightDistance, float interactionDistance, float initialSize){
-        this.timeToLive = new TimeCounter(timeToLive);
-        this.position = position;
-        this.direction = new Vector2();
-        random = new Random();
-        movesLeft = DEFAULT_MOVES;
-        setDirection();
-        this.sight = new Circle(position, sightDistance);
-        this.interaction = new Circle(position, interactionDistance);
-        speedModifier = 1.0f;
-        currentState = State.NORMAL;
-        size = initialSize;
-    }
-
-    private void setDirection(){
-        direction.set(random.nextInt(3) - 1, random.nextInt(3) - 1).nor();
-    }
-
-    @Override
-    public GeometricElement getBounds(){
-        return interaction;
     }
 
     @Override
     public void update(List<GameElement> others, float timeDifference){
 
-        if(currentState == State.GROWING){
-            movesLeft--;
-            size *= 1.15f;
-
-            if(movesLeft <= 0) {
-                currentState = State.EVOLVED;
-                size *= 0.75f;
-            }
-
-            return;
-        }
-
-        if(movesLeft <= 0){
-            movesLeft = DEFAULT_MOVES;
-            setDirection();
-            direction.mul(speedModifier);
-        }
+        behavior.age(timeDifference);
 
         for(GameElement e : others) {
             if(e instanceof MovingOrganism || e instanceof Trap)
@@ -103,56 +86,13 @@ public class MovingOrganism extends Organism {
             //TODO: Aqui se hace un cast porque en este momento solo existe otro tipo de organismo en el juego
             Organism o = (Organism) e;
 
-            if(interaction.containsCircle(o.interactionBox)){
-                interact(e);
-                break;
-            }
+            if(behavior.bounds.collides(o.getBounds()))
+                behavior.collide(o);
 
 
-            if (sight.containsCircle(o.interactionBox)) {
+            if (sight.collides(o.getBounds())) {
                 direction.set(new Vector2(o.interactionBox.position).sub(position).nor()).mul(speedModifier);
                 break;
-            }
-        }
-
-        position.add(direction);
-
-        if(position.x <= 0 || position.x >= GameLevels.FRUSTUM_WIDTH)
-            direction.x *= -1;
-        if(position.y <= 0 || position.y >= GameLevels.MAX_PLAYING_HEIGHT)
-            direction.y *= -1;
-
-        movesLeft--;
-        timeToLive.accum(timeDifference);
-    }
-
-
-    @Override
-    public float getSize(){
-        return size;
-    }
-
-    /**
-     * Modifica al organismo recibiendo un numero de foodPoints
-     * @param foodPoints que se agregaran a la instancia de organismo
-     */
-    private void consumeFood(int foodPoints){
-        if(foodPoints == 0)
-            return;
-        timeToLive.accum(- foodPoints * POINTS_SCALE);
-        speedModifier *= foodPoints * POINTS_SPEED;
-        foodConsumed += foodPoints;
-    }
-
-    @Override
-    public void interact(GameElement other){
-        if(other instanceof Organism){
-            Organism o = (Organism) other;
-            consumeFood(o.takeFood());
-
-            if(currentState == State.NORMAL && foodConsumed > FOOD_TO_EVOLVE) {
-                currentState = State.GROWING;
-                movesLeft = DEFAULT_MOVES;
             }
         }
     }
@@ -163,20 +103,5 @@ public class MovingOrganism extends Organism {
             return GameIds.EVOLVED_ORGANISM_ID;
         else
             return GameIds.MOVING_ORGANISM_ID;
-    }
-
-    @Override
-    public boolean vivo() {
-        return !timeToLive.completed();
-    }
-
-    @Override
-    public Vector2 getPosition() {
-        return position;
-    }
-
-    @Override
-    public float getPctAlive() {
-        return 1 - timeToLive.pctCharged();
     }
 }
