a6fbe7181cefc72a17c78670846e1010da81405a
jose <centaurkhan@gmail.com>
2018-02-16 12:08:50 -0600

    Se continuo con la implementacion de EatingEnemy

--- app/src/main/java/com/jgame/elements/EatingEnemy.java
+++ app/src/main/java/com/jgame/elements/EatingEnemy.java
@@ -2,11 +2,13 @@ package com.jgame.elements;
 
 import com.jgame.util.CollisionObject;
 import com.jgame.util.Decoration;
+import com.jgame.util.FrameCounter;
 import com.jgame.util.Square;
 import com.jgame.util.TextureDrawer;
 import com.jgame.util.Vector2;
 
 import java.util.ArrayDeque;
+import java.util.Random;
 
 /**
  * Created by Jose on 13/02/2018.
@@ -14,31 +16,110 @@ import java.util.ArrayDeque;
 
 public class EatingEnemy extends GameCharacter {
 
+    private enum State {
+        IDLE, EATING, ATTACKING
+    }
+
     private final byte ID_TOOL = 0;
     private final byte ID_ENEMY = 1;
     private final byte ID_PROJECTILE = 2;
-    private final static float IDLE_SIZE = 50;
+    private final static float IDLE_SIZE = 75;
     private final static TextureDrawer.TextureData IDLE_SPRITE = new TextureDrawer.TextureData(0.4375f, 0, 0.46875f, 0.09375f);
     private final static Vector2 INITIAL_POSITION = new Vector2(425, 0);
+    private final byte PROJECTILE_INITIAL_HP = 120;
+    private final Vector2 PROJECTILE_SPEED = new Vector2(-3,0.5f);
     private final Vector2 artifactPosition = new Vector2();
     private final Vector2 hitboxPosition = new Vector2();
     private final Vector2 projectilePosition = new Vector2();
+    private final TextureDrawer.ColorData EATING_COLOR = new TextureDrawer.ColorData(1,0,1,1);
+    private final TextureDrawer.ColorData ATTACKING_COLOR = new TextureDrawer.ColorData(1,1,0,1);
     private final CollisionObject.IdCollisionObject coArtifact = new CollisionObject.IdCollisionObject(new Square(artifactPosition, 50, 50),
             CollisionObject.TYPE_HITTABLE, ID_TOOL);
     private final CollisionObject.IdCollisionObject coCharacter = new CollisionObject.IdCollisionObject(new Square(hitboxPosition, 50, 50),
             CollisionObject.TYPE_HITTABLE, ID_ENEMY);
     private final CollisionObject.IdCollisionObject coProjectile = new CollisionObject.IdCollisionObject(new Square(projectilePosition, 50, 50),
             CollisionObject.TYPE_MIXED, ID_PROJECTILE);
+    private byte projectileHp;
+    private Random nextMove;
+    private State currentState;
+    private FrameCounter idleTime;
+    private FrameCounter attackStartup;
+    private boolean activeProjectile;
 
     public EatingEnemy() {
         super(new Square(new Vector2(), IDLE_SIZE, IDLE_SIZE));
         idleSizeX = IDLE_SIZE;
         baseX.x = -1;
         collisionObjects = new CollisionObject[]{coArtifact, coCharacter, coProjectile};
+        idleTime = new FrameCounter(25);
+        attackStartup = new FrameCounter(25);
+        nextMove = new Random();
+    }
+
+    /**
+     * Actualiza el color del personaje dependiendo del estado en el que se encuentre
+     */
+    private void updateColor(){
+        if(currentState == State.EATING) {
+            color.r = EATING_COLOR.r;
+            color.g = EATING_COLOR.g;
+            color.b = EATING_COLOR.b;
+        } else if (currentState == State.ATTACKING){
+            color.r = ATTACKING_COLOR.r;
+            color.g = ATTACKING_COLOR.g;
+            color.b = ATTACKING_COLOR.b;
+        } else {
+            color.r = 1;
+            color.g = 1;
+            color.b = 1;
+        }
+
     }
 
     @Override
     public void update(GameCharacter foe, ArrayDeque<Decoration> decorationData) {
+        if(currentState == State.IDLE){
+            idleTime.updateFrame();
+            if(idleTime.completed()) {
+                currentState = nextMove.nextInt(2) == 0 ? State.EATING : State.ATTACKING;
+                attackStartup.reset();
+                updateColor();
+            }
+        } else {
+            attackStartup.updateFrame();
+            if(!attackStartup.completed())
+                return;
+            if(currentState == State.EATING){
+                currentState = State.IDLE;
+            }
+
+            if(currentState == State.ATTACKING){
+                projectilePosition.set(position);
+                projectilePosition.add(-IDLE_SIZE,0);
+                activeProjectile = true;
+                projectileHp = PROJECTILE_INITIAL_HP;
+                currentState = State.IDLE;
+            }
+
+            updateColor();
+            idleTime.reset();
+        }
+
+        if(activeProjectile){
+            projectilePosition.add(PROJECTILE_SPEED);
+            projectileHp--;
+            if(projectileHp <= 0) {
+                activeProjectile = false;
+                projectilePosition.set(0,0);
+            }
+
+            if(coProjectile.bounds.collides(coArtifact.bounds)) {
+                artifactPosition.add(-3, 0);
+                activeProjectile = false;
+                projectilePosition.set(0,0);
+            }
+        }
+
     }
 
     @Override
@@ -52,6 +133,9 @@ public class EatingEnemy extends GameCharacter {
         hitboxPosition.set(position);
         artifactPosition.set(position);
         artifactPosition.add(-200, 0);
+        idleTime.reset();
+        attackStartup.reset();
+        currentState = State.IDLE;
     }
 
     @Override
