320245b7e6457cd965f785caa1da5dc58e217417
jose <centaurkhan@gmail.com>
2018-03-02 12:04:26 -0600

    Se agrego mas funcionalidad a ProjectileEnemy

--- app/src/main/java/com/jgame/elements/ProjectileEnemy.java
+++ app/src/main/java/com/jgame/elements/ProjectileEnemy.java
@@ -8,6 +8,7 @@ import com.jgame.util.TextureDrawer;
 import com.jgame.util.Vector2;
 
 import java.util.ArrayDeque;
+import java.util.Random;
 
 /**
  * Created by jose on 27/02/18.
@@ -18,6 +19,7 @@ public class ProjectileEnemy extends GameCharacter {
     private final static TextureDrawer.TextureData IDLE_SPRITE = new TextureDrawer.TextureData(0.4375f, 0, 0.46875f, 0.09375f);
     private final static float IDLE_SIZE_X = 50;
     private final static float IDLE_SIZE_Y = 100;
+    private final float PROJECTILE_WIDTH = 50;
     private final byte ID_TOOL = 0;
     private final byte ID_ENEMY = 1;
     private final byte ID_PROJECTILE = 2;
@@ -32,51 +34,86 @@ public class ProjectileEnemy extends GameCharacter {
     private final Vector2 userProjectilePosition = new Vector2();
     private final CollisionObject.IdCollisionObject coArtifact = new CollisionObject.IdCollisionObject(new Square(artifactPosition, 65, 50),
             CollisionObject.TYPE_HITTABLE, ID_TOOL);
-    private final CollisionObject.IdCollisionObject coProjectile = new CollisionObject.IdCollisionObject(new Square(projectilePosition, 50, 50),
+    private final CollisionObject.IdCollisionObject coProjectile = new CollisionObject.IdCollisionObject(new Square(projectilePosition, PROJECTILE_WIDTH, 50),
             CollisionObject.TYPE_MIXED, ID_PROJECTILE);
     private final CollisionObject.IdCollisionObject coUserProjectile = new CollisionObject.IdCollisionObject(new Square(userProjectilePosition, 50, 50),
             CollisionObject.TYPE_ATTACK, ID_USER_PROJECTILE);
     private boolean projectileLaunched;
     private boolean userProjectileLaunched;
-    private boolean alive;
-    private FrameCounter projectileInterval;
+    private FrameCounter idleInterval;
+    private FrameCounter attackStartup;
+    private State currentState;
+    private Random random;
+
+    private enum State {
+        ATTACK_A, ATTACK_B, IDLE, DEAD
+    }
 
     public ProjectileEnemy(){
         super(new Square(new Vector2(), IDLE_SIZE_X, IDLE_SIZE_Y));
-        projectileInterval = new FrameCounter(75);
+        attackStartup = new FrameCounter(20);
+        idleInterval = new FrameCounter(78);
         collisionObjects = new CollisionObject[]{coArtifact, coProjectile, coUserProjectile};
         baseX.x = -1;
+        random = new Random();
     }
 
     @Override
     public void update(GameCharacter foe, ArrayDeque<Decoration> decorationData) {
-        projectileInterval.updateFrame();
-        if(projectileInterval.completed()){
-            projectilePosition.set(position);
-            projectileLaunched = true;
+
+        if(!projectileLaunched && currentState == State.IDLE) {
+            idleInterval.updateFrame();
+            if(idleInterval.completed()){
+                currentState = random.nextInt(5) > 3 ? State.ATTACK_B : State.ATTACK_A;
+                if(currentState == State.ATTACK_A){
+                    color.g = 0;
+                    color.b = 0;
+                } else {
+                    color.r = 0;
+                    color.g = 0;
+                }
+                attackStartup.reset();
+            }
+        } else if(currentState == State.ATTACK_A){
+            attackStartup.updateFrame();
+            if(attackStartup.completed()){
+                color.g = 1;
+                color.b = 1;
+                projectilePosition.set(position);
+                projectileLaunched = true;
+                currentState = State.IDLE;
+                idleInterval.reset();
+            }
+        } else if(currentState == State.ATTACK_B){
+            attackStartup.updateFrame();
+            if(attackStartup.completed()){
+                color.r = 1;
+                color.g = 1;
+                currentState = State.IDLE;
+                idleInterval.reset();
+            }
         }
 
         if(userProjectileLaunched) {
             userProjectilePosition.add(USER_PROJECTILE_SPEED);
-            if(projectileLaunched && userProjectilePosition.x >= projectilePosition.x){
+            if(projectileLaunched && userProjectilePosition.x >= projectilePosition.x - PROJECTILE_WIDTH){
                 userProjectilePosition.set(HIDE_POSITION);
                 projectilePosition.set(HIDE_POSITION);
                 userProjectileLaunched = false;
                 projectileLaunched = false;
-                projectileInterval.reset();
             }
 
             else if(userProjectilePosition.x > position.x) {
                 color.a = 0;
                 userProjectilePosition.set(HIDE_POSITION);
                 userProjectileLaunched = false;
-                alive = false;
+                currentState = State.DEAD;
             }
         }
+
         if(projectileLaunched) {
             projectilePosition.add(PROJECTILE_SPEED);
-            if(projectilePosition.x < foe.position.x){
-                projectileInterval.reset();
+            if(projectilePosition.x - PROJECTILE_WIDTH < foe.position.x){
                 projectilePosition.set(HIDE_POSITION);
                 projectileLaunched = false;
                 foe.hit(coProjectile);
@@ -93,12 +130,12 @@ public class ProjectileEnemy extends GameCharacter {
     public void reset(Vector2 positionOffset) {
         projectileLaunched = false;
         userProjectileLaunched = false;
-        projectileInterval.reset();
+        idleInterval.reset();
         moveTo(positionOffset, INITIAL_POSITION);
         artifactPosition.set(position);
         artifactPosition.add(ARTIFACT_INITIAL_OFFSET);
         userProjectilePosition.set(HIDE_POSITION);
-        alive = true;
+        currentState = State.IDLE;
     }
 
     @Override
@@ -108,7 +145,7 @@ public class ProjectileEnemy extends GameCharacter {
 
     @Override
     public boolean alive() {
-        return alive;
+        return currentState != State.DEAD;
     }
 
     @Override
